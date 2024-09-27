da075054f62e0c387ba775ec55f3b65d4e9bea72
Shahin Sarker <141581883+Shahin4292@users.noreply.github.com>
2024-09-27 17:18:38 +0600

    put api screen text from field add done

---
+++ lib/Screen/put_api_screen.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+class PutApiScreen extends StatefulWidget {
+  const PutApiScreen({super.key});
+
+  @override
+  State<PutApiScreen> createState() => _PutApiScreenState();
+}
+
+class _PutApiScreenState extends State<PutApiScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text("Put Api"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              decoration: InputDecoration(
+                hintText: "Name",
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(12),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            TextField(
+              decoration: InputDecoration(
+                hintText: "Job",
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(12),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blue,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  fixedSize: Size(MediaQuery.sizeOf(context).width, 50),
+                ),
+                onPressed: () {},
+                child: const Text(
+                  "Update",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18,
+                      color: Colors.white),
+                ))
+          ],
+        ),
+      ),
+    );
+  }
+}
