4dc8638b831be2213f59e22ccb08f87628339b89
Shahin Sarker <141581883+Shahin4292@users.noreply.github.com>
2024-09-28 07:49:19 +0600

    Service api page done

---
+++ lib/Service/put_api_service.dart
@@ -0,0 +1,31 @@
+import 'dart:convert';
+
+import '../Model/update_model.dart';
+import 'package:http/http.dart' as http;
+
+class PutApiService {
+  Future<UpdateModel?> updateData(String name, String job) async {
+    var url = Uri.parse("https://reqres.in/api/users/2");
+    var response = await http.put(url, body: {
+      "name": name,
+      "job": job,
+    });
+    try {
+      if (response.statusCode == 200) {
+        UpdateModel model = UpdateModel.fromJson(
+            jsonDecode(response.body) as Map<String, dynamic>);
+        print(response.body);
+        print("Data Update Successfully");
+        return model;
+      }
+    } catch (e) {
+      print(e.toString());
+    }
+    return null;
+  }
+}
+
+// {
+// "name": "morpheus",
+// "job": "zion resident"
+// }
